c3c9b66c56d71709fe86717be08c6b934cb78d25
Anna Konopka <ania@zdwola.pl>
2016-11-05 23:40:34 +0100

    Metrics + start of router

---
+++ src/components/Metrics.vue
@@ -0,0 +1,33 @@
+<template>
+  <main>
+    <span v-for="(val, key) in props">{{key}} - {{val}}</span>
+  </main>
+</template>
+
+<script>
+import 'clientjs';
+
+export default {
+  name: 'metrics',
+  data() {
+    return {
+      client: new window.ClientJS(),
+    };
+  },
+  computed: {
+    props: function props() {
+      return Object.keys(window.ClientJS.prototype).reduce((acc, key) => ({
+        ...acc,
+        [key]: this.client[key](),
+      }), {});
+    },
+  },
+};
+</script>
+
+<style scoped>
+main {
+  display: flex;
+  flex-direction: column;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,33 @@
 import Vue from 'vue';
-import App from './App';
+import Metrics from './components/Metrics';
+import Devices from './components/Devices';
+
+const routes = {
+  '/': 'Metrics',
+  '/devices': 'Devices',
+};
 
 /* eslint-disable no-new */
-new Vue({
+const app = new Vue({
   el: '#app',
-  template: '<App/>',
-  components: { App },
+  data: {
+    currentRoute: window.location.pathname,
+  },
+  components: {
+    Metrics,
+    Devices,
+  },
+  computed: {
+    ViewComponent() {
+      return routes[this.currentRoute];
+    },
+  },
+  render(h) {
+    return h(this.ViewComponent);
+  },
+});
+
+
+window.addEventListener('popstate', () => {
+  app.currentRoute = window.location.pathname;
 });
